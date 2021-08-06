07e1daa0a963d2517a51aa7aca72699e3d997a8e
Thamidu Anjana <pmatabandara@students.nsbm.ac.lk>
2021-08-06 15:18:17 +0530

    Modify Table

--- library_management_system/src/App.js
+++ library_management_system/src/App.js
@@ -2,13 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Button from 'react-bootstrap/Button';
-import {Button,Container,Row,Col} from 'react-bootstrap';
+import { Button, Container, Row, Col, Table } from 'react-bootstrap';
 
 function App() {
   return (
-    <Container fluid className ="Body">
+    <Container fluid className="Body">
       <Row>
-        <Col><h1>Library Management System..</h1></Col>
+        <Col className="left_margin top_padding"><h1>Library Management System..</h1></Col>
+      </Row>
+      <Row>
+        <Col lg="6" className="top_padding left_margin">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan="2">Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
       </Row>
     </Container>
   );
