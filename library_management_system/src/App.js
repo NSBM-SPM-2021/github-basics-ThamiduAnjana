2beacf22fdab8c921e89860bf4252500bb189a0e
Thamidu Anjana <pmatabandara@students.nsbm.ac.lk>
2021-08-06 20:18:36 +0530

    add

--- library_management_system/src/App.js
+++ library_management_system/src/App.js
@@ -18,7 +18,6 @@ function App() {
                 <th>#</th>
                 <th>First Name</th>
                 <th>Last Name</th>
-                <th>Username</th>
               </tr>
             </thead>
             <tbody>
@@ -26,18 +25,15 @@ function App() {
                 <td>1</td>
                 <td>Mark</td>
                 <td>Otto</td>
-                <td>@mdo</td>
               </tr>
               <tr>
                 <td>2</td>
                 <td>Jacob</td>
                 <td>Thornton</td>
-                <td>@fat</td>
               </tr>
               <tr>
                 <td>3</td>
                 <td colSpan="2">Larry the Bird</td>
-                <td>@twitter</td>
               </tr>
             </tbody>
           </Table>
