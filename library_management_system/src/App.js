f7eab85ea4da78aa5374662e7133a2cf02f6eacf
Thamidu Anjana <pmatabandara@students.nsbm.ac.lk>
2021-08-07 11:02:04 +0530

    Add two input fields

--- library_management_system/src/App.js
+++ library_management_system/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Button from 'react-bootstrap/Button';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, Form } from 'react-bootstrap';
 
 function App() {
   return (
@@ -11,17 +11,33 @@ function App() {
         <Col className="text_align_center top_padding_3"><h1>Library Management System..</h1></Col>
       </Row>
       <Row>
-        <Col lg="6" className="top_padding_3 align_center">
-          <Card style={{ width: '100%' }}>
-            <Card.Body>
-              <Card.Title className="text_align_center">Add New Books In Library</Card.Title>
-              <hr></hr>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Button variant="primary">Save</Button>
-            </Card.Body>
+        <Col lg="8" className="top_padding_3 align_center">
+          <Card style={{ width: '100%',padding:'15px'}}>
+            <Form>
+              <Card.Body>
+                <Card.Title className="text_align_center">Add New Books In Library</Card.Title>
+                <hr></hr>
+                <Card.Text>
+                  <Row>
+                    <Col>
+                      <Form.Group controlId="bookid">
+                        <Form.Label>Book ID :</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Book ID"></Form.Control>
+                      </Form.Group>
+                    </Col>
+                    <Col>
+                    <Form.Group controlId="isbnno">
+                        <Form.Label>ISBN NO :</Form.Label>
+                        <Form.Control type="text" placeholder="Enter ISBN NO"></Form.Control>
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                </Card.Text>
+                <div className="buttom_align_right">
+                  <Button variant="primary" className="button_style">Save Book</Button>
+                </div>
+              </Card.Body>
+            </Form>
           </Card>
         </Col>
       </Row>
