ea8a71b91624ebb81b6f6a9ed4f5530893ab15a3
Thamidu Anjana <pmatabandara@students.nsbm.ac.lk>
2021-08-09 10:22:58 +0530

    Modify form

--- library_management_system/src/App.js
+++ library_management_system/src/App.js
@@ -7,7 +7,16 @@ import { useState } from 'react';
 
 function App() {
 
-const[bookid, setbookid] = useState("");
+  const [txt_bookid, setbookid] = useState("");
+  const [txt_isbnno, setisbnno] = useState("");
+  const [txt_booktitle, setbooktitle] = useState("");
+  const [txt_author, setauthor] = useState("");
+  const [txt_publishdate, setpublishdate] = useState("");
+  const [txt_addingdate, setaddingdate] = useState("");
+  const [txt_pages, setpages] = useState("");
+  const [txt_price, setprice] = useState("");
+  const [txt_source, setsource] = useState("");
+  const [txt_remarks, setremarks] = useState("");
 
   return (
     <Container fluid className="Body">
@@ -16,7 +25,7 @@ const[bookid, setbookid] = useState("");
       </Row>
       <Row>
         <Col lg="8" className="top_padding_1 align_center">
-          <Card style={{ width: '100%',padding:'15px'}}>
+          <Card style={{ width: '100%', padding: '15px' }}>
             <Form>
               <Card.Body>
                 <Card.Title className="text_align_center">Add New Books In Library</Card.Title>
@@ -26,13 +35,13 @@ const[bookid, setbookid] = useState("");
                     <Col>
                       <Form.Group controlId="bookid">
                         <Form.Label>Book ID :</Form.Label>
-                        <Form.Control type="text" name="bookid" placeholder="Enter Book ID"></Form.Control>
+                        <Form.Control type="text" name="txt_bookid" placeholder="Enter Book ID"></Form.Control>
                       </Form.Group>
                     </Col>
                     <Col>
-                    <Form.Group controlId="isbnno">
+                      <Form.Group controlId="isbnno">
                         <Form.Label>ISBN NO :</Form.Label>
-                        <Form.Control type="text" name="isbnno" placeholder="Enter ISBN NO"></Form.Control>
+                        <Form.Control type="text" name="txt_isbnno" placeholder="Enter ISBN NO"></Form.Control>
                       </Form.Group>
                     </Col>
                   </Row>
@@ -40,13 +49,13 @@ const[bookid, setbookid] = useState("");
                     <Col>
                       <Form.Group controlId="booktitle">
                         <Form.Label>Book Title :</Form.Label>
-                        <Form.Control type="text" name="booktitle" placeholder="Enter Book Title"></Form.Control>
+                        <Form.Control type="text" name="txt_booktitle" placeholder="Enter Book Title"></Form.Control>
                       </Form.Group>
                     </Col>
                     <Col>
-                    <Form.Group controlId="author">
+                      <Form.Group controlId="author">
                         <Form.Label>Author :</Form.Label>
-                        <Form.Control type="text" name="author" placeholder="Enter Author"></Form.Control>
+                        <Form.Control type="text" name="txt_author" placeholder="Enter Author"></Form.Control>
                       </Form.Group>
                     </Col>
                   </Row>
@@ -54,13 +63,13 @@ const[bookid, setbookid] = useState("");
                     <Col>
                       <Form.Group controlId="publishdate">
                         <Form.Label>Publish Date :</Form.Label>
-                        <Form.Control type="date" name="publishdate" placeholder="Enter Publish Date"></Form.Control>
+                        <Form.Control type="date" name="txt_publishdate" placeholder="Enter Publish Date"></Form.Control>
                       </Form.Group>
                     </Col>
                     <Col>
-                    <Form.Group controlId="addingdate">
+                      <Form.Group controlId="addingdate">
                         <Form.Label>Adding Date :</Form.Label>
-                        <Form.Control type="date" name="addingdate" placeholder="Enter Adding Date"></Form.Control>
+                        <Form.Control type="date" name="txt_addingdate" placeholder="Enter Adding Date"></Form.Control>
                       </Form.Group>
                     </Col>
                   </Row>
@@ -68,13 +77,13 @@ const[bookid, setbookid] = useState("");
                     <Col>
                       <Form.Group controlId="pages">
                         <Form.Label>Pages :</Form.Label>
-                        <Form.Control type="text" name="pages" placeholder="Enter Pages"></Form.Control>
+                        <Form.Control type="text" name="txt_pages" placeholder="Enter Pages"></Form.Control>
                       </Form.Group>
                     </Col>
                     <Col>
-                    <Form.Group controlId="price">
+                      <Form.Group controlId="price">
                         <Form.Label>Price :</Form.Label>
-                        <Form.Control type="text" name="price" placeholder="Enter Price"></Form.Control>
+                        <Form.Control type="text" name="txt_price" placeholder="Enter Price"></Form.Control>
                       </Form.Group>
                     </Col>
                   </Row>
@@ -82,13 +91,13 @@ const[bookid, setbookid] = useState("");
                     <Col>
                       <Form.Group controlId="source">
                         <Form.Label>Source :</Form.Label>
-                        <Form.Control as="textarea" name="source" rows={3} placeholder="Enter Source"></Form.Control>
+                        <Form.Control as="textarea" name="txt_source" rows={3} placeholder="Enter Source"></Form.Control>
                       </Form.Group>
                     </Col>
                     <Col>
-                    <Form.Group controlId="remark">
+                      <Form.Group controlId="remark">
                         <Form.Label>Remark :</Form.Label>
-                        <Form.Control as="textarea" name="remarks" rows={3} placeholder="Enter Remark"></Form.Control>
+                        <Form.Control as="textarea" name="txt_remarks" rows={3} placeholder="Enter Remark"></Form.Control>
                       </Form.Group>
                     </Col>
                   </Row>
