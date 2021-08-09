275af913e1714a39cd9e3342f8197b6625128bd9
Thamidu Anjana <pmatabandara@students.nsbm.ac.lk>
2021-08-09 11:15:26 +0530

    modify back-end code

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
 
@@ -9,10 +11,33 @@ const db = mysql.createPool({
     database: "spm_librarymanagementsystem"
 });
 
-app.get("/", (req,res) => {
-    const sqlInsert = "INSERT INTO books_tb (Book_ID,ISBN_NO,Book_Title,Author,Publish_Date,Adding_Date,Pages,Price,Source,Remarks)VALUES(1,'123','test','test','2021-8-8','2021-8-8',500,555.75,'test','test');";
-    db.query(sqlInsert, (err, result)=>{
-        res.send("hello world hi.");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/api/get", (req,res) =>{
+    const sqlSelect = "SELECT * FROM books_tb";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+});
+
+app.post("/api/insert", (req,res) => {
+
+    const bookid = req.body.bookid;
+    const isbnno = req.body.isbnno;
+    const booktitle = req.body.booktitle;
+    const author = req.body.author;
+    const publishdate = req.body.publishdate;
+    const addingdate = req.body.addingdate;
+    const pages = req.body.pages;
+    const price = req.body.price;
+    const source = req.body.source;
+    const remarks = req.body.remarks;
+
+    const sqlInsert = "INSERT INTO books_tb (Book_ID,ISBN_NO,Book_Title,Author,Publish_Date,Adding_Date,Pages,Price,Source,Remarks)VALUES(?,?,?,?,?,?,?,?,?,?)";
+    db.query(sqlInsert, [bookid,isbnno,booktitle,author,publishdate,addingdate,pages,price,source,remarks],(err, result) => {
+        console.log(result);
     });
 });
 
