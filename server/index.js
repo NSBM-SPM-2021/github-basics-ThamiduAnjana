2fb7e2068b16688f711eeb446204f49e6e54b963
Thamidu Anjana <pmatabandara@students.nsbm.ac.lk>
2021-08-09 08:48:49 +0530

    Connect mysql database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
-// const mysql = require("mysql");
-
 const app = express();
+//const mysql = require("mysql");
+
+/*const db = mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"spm_librarymanagementsystem",
+});*/
 
 app.get("/",(req, res) => {
-    res.send("hello world");
+    /*const sqlInsert = "INSERT INTO books_tb (Book_ID,ISBN_NO,Book_Title,Author,Publish_Date"-
+    ",Adding_Date,Pages,Source,Remarks) VALUES (1,'12','TEST','TEST','2021-08-08','2021-08-08'"-
+    ",50,550.00,'TEST','TEST');"
+    db.query(sqlInsert,(err,result)=>{*/
+        res.send("hello world");
+    //});
 });
 
 app.listen(3001,() => {
